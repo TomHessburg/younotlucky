fc31c4c6eef8b772cf419ce0a6b18e8b9249f95b
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-11-23 02:38:36 -0500

    i lied, added a small section to landing page, nothing functional just some text :D

--- Frontend/younotlucky/src/components/LandingHeader.js
+++ Frontend/younotlucky/src/components/LandingHeader.js
@@ -10,33 +10,56 @@ export default function LandingHeader() {
   return (
     <div>
       <Wrapper>
-        <h1>YOU NO LUCKY, JalYt!</h1>
-        <img style={{ width: "240px" }} src={Jad} alt="Jad" />
+        <img style={{ width: "120px" }} src={Jad} alt="Jad" />
       </Wrapper>
-      <StyledPs>
-        You ever wonder exactly HOW unlucky you are going 3x drop rate for that
-        pet?
-      </StyledPs>
-      <StyledPs>Welp, lets find out EXACTLY HOW unlucky you are :D</StyledPs>
-      <Link
-        to="/find_monster"
-        style={{ textDecoration: "none", color: "white" }}
-      >
-        <Button
-          size="large"
-          variant="contained"
-          color="secondary"
-          style={{ marginBottom: "64px" }}
-        >
-          Check Your Luck <ArrowForwardIcon style={{ marginLeft: "12px" }} />
-        </Button>
-      </Link>
+      <h1>
+        You No Lucky! Find out how ridiculous that OSRS dry streak really is.
+      </h1>
+      <BottomContainerWrappers>
+        <ParagraphContainer>
+          <p>
+            You ever wonder exactly HOW unlucky you are going 3x drop rate for
+            that pet? Welp, lets find out EXACTLY how unlucky you are :D
+          </p>
+          <Link
+            to="/find_monster"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            <Button
+              size="medium"
+              variant="contained"
+              color="secondary"
+              style={{ marginBottom: "64px" }}
+            >
+              Check Your Luck{" "}
+              <ArrowForwardIcon style={{ marginLeft: "12px" }} />
+            </Button>
+          </Link>
+        </ParagraphContainer>
+        <ParagraphContainer>
+          <p>
+            <strong>SO YOU KNOW</strong> I'm definitely not a statistics major
+            or anything. Just figured i'd make this little tool that uses some
+            basic binomial probability to give you some kind of an estimate of
+            how ridiculously unlucky you are :) If you notice anything wrong,
+            please lmk!
+          </p>
+          <Button
+            size="medium"
+            variant="contained"
+            color="secondary"
+            style={{ marginBottom: "64px" }}
+          >
+            Contact Me <ArrowForwardIcon style={{ marginLeft: "12px" }} />
+          </Button>
+        </ParagraphContainer>
+      </BottomContainerWrappers>
     </div>
   );
 }
 
 const Wrapper = styled.div`
-  padding: 80px 0;
+  padding: 40px 0;
   width: 100%;
   heigh: 100vh;
   background: #eaeaea;
@@ -46,7 +69,26 @@ const Wrapper = styled.div`
   }
 `;
 
-const StyledPs = styled.p`
-  color: #0c0c0c;
-  font-size: 24px;
+const ParagraphContainer = styled.div`
+  max-width: 450px;
+  margin: auto;
+  padding: 0 16px;
+
+  p {
+    font-size: 20px;
+    color: #0c0c0c;
+    max-width: 600px;
+    text-align: left;
+  }
+`;
+
+const BottomContainerWrappers = styled.div`
+  display: flex;
+  align-items: center;
+  justify-conent: center:
+  flex-wrap: nowrap;
+  margin-top: 64px;
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
 `;
